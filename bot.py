00fc9909fdc2340dad28f7eca941c054b1b35452
Svetlana Polyanskaya <s.s.polyanskaya@gmail.com>
2023-03-20 10:28:17 +0300

    Added bot answer for the case of a non-existent city

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ from weather import Weather
 logging.basicConfig(level=logging.INFO)
 
 
-BOT_API_TOKEN = 'XXXXXXXXXXXXXXXXXXXX' # a valid telegram bot token provided by BotFather
-WEATHER_API_KEY = 'YYYYYYYYYYYYYYYYYYYYY' # a valid open weather map API token
+BOT_API_TOKEN = 'XXXXXXXXXXXX' # a valid telegram bot token provided by BotFather
+WEATHER_API_KEY = 'YYYYYYYYYYYYYYYY' # a valid open weather map API token
 
 
 bot = Bot(token=BOT_API_TOKEN)
@@ -22,21 +22,24 @@ def get_weather(text):
         temperature = Weather.get_temperature(extended_weather)
         return [main_weather, temperature]
     except KeyError:
-        print('City name is absent in the database.')
+        print('KeyError: city name is absent in the database.')
 
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-   await message.reply("Hi!\nType city name to see the weather.")
+   await message.reply("Hi! Type a city name to see the weather.")
 
 
 @dp.message_handler()
 async def text_message(message: types.Message):
     try:
         weather_list = get_weather(message.text)
-        await message.answer(f"{weather_list[0]}\n{weather_list[1]}")
+        if weather_list:
+            await message.answer(f"{weather_list[0]}\n{weather_list[1]}")
+        else:
+            await message.answer("Looks like this city is absent in our database. Check spelling or type the nearest city.")
     except MessageTextIsEmpty:
-        print('City name is absent in the database.')
+        print('MessageTextIsEmpty: city name is absent in the database.')
 
 
 if __name__ == '__main__':
